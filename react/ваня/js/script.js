540e4d7e4a876ada41e094ac5bd8fca29e8ebad0
Denis <denmyakishev@mail.ru>
2023-05-16 11:55:13 +0300

    shit

--- react/ваня/js/script.js
+++
@@ -1,4 +0,0 @@
-import "./loader.js"
-import  "./slider.js";
-import "./header.js";
-import "./swiper.js";
--- react/ваня/js/swiper.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper(`.swiper`, {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  observeSlideChildren: true,
-  grabCursor: true,
-  loop: true,
-});
